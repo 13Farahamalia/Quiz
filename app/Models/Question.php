cce359f3506bd51b739a57b1302995ebc8e487fc
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-02-07 14:50:26 +0700

    model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Question extends Model
 {
     use HasFactory;
+
+    public function quiz(): BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
+
+    public function choices(): HasMany
+    {
+        return $this->hasMany(Choice::class);
+    }
+
+    public function answer(): HasOne
+    {
+        return $this->hasOne(Answer::class);
+    }
+
+    public function choice(): HasOne
+    {
+        return $this->hasOne(Choice::class);
+    }
 }
